5f19ffee3dbc78faf478cbdd3460b560b4d8c335
jonpetersen <jonepetersen@gmail.com>
2024-02-10 13:58:09 +0000

    changes to gallery

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,13 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
   const gridItems = document.querySelectorAll(".grid-item");
+  const overlay = document.getElementById("overlay");
+  const modal = document.getElementById("modal");
+  const expandedImage = document.getElementById("expanded-image");
+  const closeModal = document.getElementById("close-modal");
 
   gridItems.forEach((item) => {
     item.addEventListener("click", function () {
-      this.querySelector("img").style.transform = "scale(4)";
+      const clickedImageSrc = this.querySelector("img").src;
+      expandedImage.src = clickedImageSrc;
+      overlay.style.display = "block";
     });
+  });
 
-    item.addEventListener("mouseleave", function () {
-      this.querySelector("img").style.transform = "scale(1)";
-    });
+  closeModal.addEventListener("click", function () {
+    overlay.style.display = "none";
   });
 });
