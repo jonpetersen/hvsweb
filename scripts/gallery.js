ebbbeaa971182d1e328f067b3badcda34e06e7a2
jonpetersen <jonepetersen@gmail.com>
2024-02-10 11:56:35 +0000

    made gallery grid

---
+++ scripts/gallery.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const gridItems = document.querySelectorAll(".grid-item");
+
+  gridItems.forEach((item) => {
+    item.addEventListener("click", function () {
+      this.querySelector("img").style.transform = "scale(1.5)";
+    });
+
+    item.addEventListener("mouseleave", function () {
+      this.querySelector("img").style.transform = "scale(1)";
+    });
+  });
+});
