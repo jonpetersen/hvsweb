2969df7e52b648af55afe826ac8a5a66c4b9c2c7
jonpetersen <jonepetersen@gmail.com>
2024-02-10 14:48:59 +0000

    added navigationto gallery and more photos

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -4,9 +4,14 @@ document.addEventListener("DOMContentLoaded", function () {
   const modal = document.getElementById("modal");
   const expandedImage = document.getElementById("expanded-image");
   const closeModal = document.getElementById("close-modal");
+  const prevBtn = document.getElementById("prev-btn");
+  const nextBtn = document.getElementById("next-btn");
 
-  gridItems.forEach((item) => {
+  let currentIndex = 0;
+
+  gridItems.forEach((item, index) => {
     item.addEventListener("click", function () {
+      currentIndex = index;
       const clickedImageSrc = this.querySelector("img").src;
       expandedImage.src = clickedImageSrc;
       overlay.style.display = "block";
@@ -14,6 +19,40 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
   closeModal.addEventListener("click", function () {
-    overlay.style.display = "none";
+    closeExpandedImage();
   });
+
+  overlay.addEventListener("click", function () {
+    closeExpandedImage();
+  });
+
+  modal.addEventListener("click", function (event) {
+    event.stopPropagation();
+  });
+
+  prevBtn.addEventListener("click", function (event) {
+    event.stopPropagation();
+    showPrevImage();
+  });
+
+  nextBtn.addEventListener("click", function (event) {
+    event.stopPropagation();
+    showNextImage();
+  });
+
+  function showPrevImage() {
+    currentIndex = (currentIndex - 1 + gridItems.length) % gridItems.length;
+    const prevImageSrc = gridItems[currentIndex].querySelector("img").src;
+    expandedImage.src = prevImageSrc;
+  }
+
+  function showNextImage() {
+    currentIndex = (currentIndex + 1) % gridItems.length;
+    const nextImageSrc = gridItems[currentIndex].querySelector("img").src;
+    expandedImage.src = nextImageSrc;
+  }
+
+  function closeExpandedImage() {
+    overlay.style.display = "none";
+  }
 });
